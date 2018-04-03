6214195c2e009b50ff0f5b9bce40173293ad19a8
ggxc <962616732@qq.com>
2018-04-03 16:55:29 +0800

    ..

--- app/src/main/java/cn/edu/gdmec/android/activity/MainActivity.java
+++ app/src/main/java/cn/edu/gdmec/android/activity/MainActivity.java
@@ -1,15 +1,48 @@
 package cn.edu.gdmec.android.activity;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import cn.edu.gdmec.android.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    private RelativeLayout main_body;
+    private TextView bottom_bar_text_course;
+    private ImageView bottom_bar_image_course;
+    private RelativeLayout bottom_bar_course_btn;
+    private TextView bottom_bar_text_exercise;
+    private ImageView bottom_bar_image_exercise;
+    private RelativeLayout bottom_bar_exercise_btn;
+    private TextView bottom_bar_text_myinfo;
+    private ImageView bottom_bar_image_myinfo;
+    private RelativeLayout bottom_bar_myinfo_btn;
+    private LinearLayout main_bottom_bar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+    }
+
+    private void initView() {
+        main_body = (RelativeLayout) findViewById(R.id.main_body);
+        bottom_bar_text_course = (TextView) findViewById(R.id.bottom_bar_text_course);
+        bottom_bar_image_course = (ImageView) findViewById(R.id.bottom_bar_image_course);
+        bottom_bar_course_btn = (RelativeLayout) findViewById(R.id.bottom_bar_course_btn);
+        bottom_bar_text_exercise = (TextView) findViewById(R.id.bottom_bar_text_exercise);
+        bottom_bar_image_exercise = (ImageView) findViewById(R.id.bottom_bar_image_exercise);
+        bottom_bar_exercise_btn = (RelativeLayout) findViewById(R.id.bottom_bar_exercise_btn);
+        bottom_bar_text_myinfo = (TextView) findViewById(R.id.bottom_bar_text_myinfo);
+        bottom_bar_image_myinfo = (ImageView) findViewById(R.id.bottom_bar_image_myinfo);
+        bottom_bar_myinfo_btn = (RelativeLayout) findViewById(R.id.bottom_bar_myinfo_btn);
+        main_bottom_bar = (LinearLayout) findViewById(R.id.main_bottom_bar);
     }
 }
