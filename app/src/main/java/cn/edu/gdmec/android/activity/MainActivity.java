f3b30ce3e06bfb85498457a29d0a5ff5871a4aae
ggxc <az962616732@vip.qq.com>
2018-04-08 18:08:07 +0800

    ...

--- app/src/main/java/cn/edu/gdmec/android/activity/MainActivity.java
+++ app/src/main/java/cn/edu/gdmec/android/activity/MainActivity.java
@@ -1,16 +1,27 @@
 package cn.edu.gdmec.android.activity;
 
+import android.content.Intent;
 import android.content.pm.ActivityInfo;
+import android.graphics.Color;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.view.KeyEvent;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import cn.edu.gdmec.android.Fragment.CourseFragment;
+import cn.edu.gdmec.android.Fragment.ExercisesFragment;
+import cn.edu.gdmec.android.Fragment.FragmentMyinfoFragment;
 import cn.edu.gdmec.android.R;
+import cn.edu.gdmec.android.utils.AnalysisUtils;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends FragmentActivity implements View.OnClickListener {
 
     private RelativeLayout main_body;
     private TextView bottom_bar_text_course;
@@ -24,12 +35,58 @@ public class MainActivity extends AppCompatActivity {
     private RelativeLayout bottom_bar_myinfo_btn;
     private LinearLayout main_bottom_bar;
 
+    FragmentManager manager = getSupportFragmentManager();
+    FragmentTransaction transaction = manager.beginTransaction();
+transaction.add(R.id.main_body,new CourseFragment()).commit();
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (data != null) {
+            boolean isLogin = data.getBooleanExtra("isLogin", false);
+            if (isLogin) {
+                setSelectStatus(0);
+            } else {
+                setSelectStatus(2);
+            }
+        }
+    }
+
+    protected long exitTime;
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK && event.getAction() == KeyEvent.ACTION_DOWN) {
+            if ((System.currentTimeMillis() - exitTime) > 2000) {
+                Toast.makeText(MainActivity.this, "再按一次退出博学谷", Toast.LENGTH_SHORT).show();
+                exitTime = System.currentTimeMillis();
+            }
+
+         else {
+            this.finish();
+            if (AnalysisUtils.readLoginStatus(this)) {
+                AnalysisUtils.clearLoginStatus(this);
+            }
+            System.exit(0);
+        }
+        return true;
+    }
+return super.onKeyDown(keyCode, event);
+}
+
+    private void setMain(){
+        this.getSupportFragmentManager().beginTransaction().add(R.id.main_body,new CourseFragment()).commit();
+        setSelectStatus(0);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initView();
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+        setMain();;
     }
 
     private void initView() {
@@ -44,5 +101,61 @@ public class MainActivity extends AppCompatActivity {
         bottom_bar_image_myinfo = (ImageView) findViewById(R.id.bottom_bar_image_myinfo);
         bottom_bar_myinfo_btn = (RelativeLayout) findViewById(R.id.bottom_bar_myinfo_btn);
         main_bottom_bar = (LinearLayout) findViewById(R.id.main_bottom_bar);
+
+        bottom_bar_course_btn.setOnClickListener(this);
+        bottom_bar_exercise_btn.setOnClickListener(this);
+        bottom_bar_myinfo_btn.setOnClickListener(this);
+    }
+    private  void setSelectStatus(int index){
+        switch (index){
+            case 0:
+                bottom_bar_image_course.setImageResource(R.drawable.main_course_icon_selected);
+                bottom_bar_text_course.setTextColor(Color.parseColor("#0097F7"));
+
+                bottom_bar_text_exercise.setTextColor(Color.parseColor("#66666"));
+                bottom_bar_text_myinfo.setTextColor(Color.parseColor("#66666"));
+
+                bottom_bar_image_exercise.setImageResource(R.drawable.main_exercises_icon);
+                bottom_bar_image_myinfo.setImageResource(R.drawable.main_my_icon);
+                break;
+            case 1:
+                bottom_bar_image_exercise.setImageResource(R.drawable.main_course_icon_selected);
+                bottom_bar_text_exercise.setTextColor(Color.parseColor("#0097F7"));
+
+                bottom_bar_text_course.setTextColor(Color.parseColor("#66666"));
+                bottom_bar_text_myinfo.setTextColor(Color.parseColor("#66666"));
+
+                bottom_bar_image_course.setImageResource(R.drawable.main_course_icon);
+                bottom_bar_image_myinfo.setImageResource(R.drawable.main_my_icon);
+                break;
+            case 2:
+                bottom_bar_image_myinfo.setImageResource(R.drawable.main_course_icon_selected);
+                bottom_bar_text_myinfo.setTextColor(Color.parseColor("#0097F7"));
+
+                bottom_bar_text_course.setTextColor(Color.parseColor("#66666"));
+                bottom_bar_text_exercise.setTextColor(Color.parseColor("#66666"));
+
+                bottom_bar_image_exercise.setImageResource(R.drawable.main_exercises_icon);
+                bottom_bar_image_course.setImageResource(R.drawable.main_course_icon);
+                break;
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.bottom_bar_course_btn:
+                getSupportFragmentManager().beginTransaction().replace(R.id.main_body,new CourseFragment()).commit();
+                setSelectStatus(0);
+                break;
+            case R.id.bottom_bar_exercise_btn:
+                getSupportFragmentManager().beginTransaction().replace(R.id.main_body,new ExercisesFragment()).commit();
+                setSelectStatus(1);
+                break;
+            case R.id.bottom_bar_myinfo_btn:
+                getSupportFragmentManager().beginTransaction().replace(R.id.main_body,new FragmentMyinfoFragment()).commit();
+                setSelectStatus(2);
+                break;
+        }
     }
 }
